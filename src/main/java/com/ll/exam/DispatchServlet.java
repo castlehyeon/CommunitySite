c03f761d748c2577f1897caf466d40010818b0d0
castlehyeon <qnftktls118@naver.com>
2022-07-27 10:45:38 +0900

    5강개발환경 세팅

---
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -0,0 +1,51 @@
+package com.ll.exam;
+
+import com.ll.exam.article.ArticleController;
+import com.ll.exam.member.MemberController;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/*")
+public class DispatchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        Rq rq = new Rq(req, resp);
+
+        MemberController memberController = new MemberController();
+        ArticleController articleController = new ArticleController();
+
+        switch (rq.getMethod()) {
+            case "GET":
+                switch (rq.getActionPath()) {
+                    case "/usr/article/detail":
+                        articleController.showDetail(rq);
+                        break;
+                    case "/usr/article/list":
+                        articleController.showList(rq);
+                        break;
+                    case "/usr/article/write":
+                        articleController.showWrite(rq);
+                        break;
+                    case "/usr/member/login":
+                        memberController.showLogin(rq);
+                        break;
+                }
+                break;
+            case "POST":
+                switch (rq.getActionPath()) {
+                    case "/usr/article/write":
+                        articleController.doWrite(rq);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
+    }
+}
+
---
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -0,0 +1,53 @@
+package com.ll.exam.article;
+
+import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.List;
+
+public class ArticleController {
+    private ArticleService articleService;
+
+    public ArticleController() {
+        articleService = new ArticleService();
+    }
+
+    public void showList(Rq rq) {
+        List<ArticleDto> articleDtos = articleService.findAll();
+
+        rq.setAttr("articles", articleDtos);
+        rq.view("usr/article/list");
+    }
+
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
+
+    public void doWrite(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        long id = articleService.write(title, body);
+
+        rq.appendBody("%d번 게시물이 생성 되었습니다.".formatted(id));
+    }
+
+    public void showDetail(Rq rq) {
+        long id = rq.getLongPathValueByIndex(1, 0);
+
+        if (id == 0) {
+            rq.appendBody("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.appendBody("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        rq.setAttr("article", articleDto);
+        rq.view("usr/article/detail");
+    }
+}
